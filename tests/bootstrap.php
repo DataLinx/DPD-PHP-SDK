51db5a90cec5e9ed0d7a1defd714765c6020b07b
SlimDeluxe <SlimDeluxe@users.noreply.github.com>
2023-02-20 06:57:39 +0000

    style: fix code style

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,7 @@
 <?php
 
 // Validate config
-if (empty(getenv('DPD_USERNAME')) || empty(getenv('DPD_PASSWORD')) || empty(getenv('DPD_COUNTRY_CODE')))
-{
+if (empty(getenv('DPD_USERNAME')) || empty(getenv('DPD_PASSWORD')) || empty(getenv('DPD_COUNTRY_CODE'))) {
     trigger_error('You must set the DPD_USERNAME, DPD_PASSWORD and DPD_COUNTRY_CODE env. variables before running the tests!', E_USER_ERROR);
 }
 
